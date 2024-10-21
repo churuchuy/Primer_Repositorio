5de48db68e9bbfa20fb79ebad7c9e83ad779a7b9
López Rojas Jesus Emiliano <jesus.lopez.rojas@cbtis89.edu.mx>
2024-10-21 09:11:09 -0600

    Add files via upload

---
+++ P01Variables1.php
@@ -0,0 +1,20 @@
+<?php
+$dia = 24;//Se declara una variable de tipo integro.
+$sueldo = 758.43; //Se decara una variable de tipo double.
+$nombre = "Karla"; //Se declara una variable de tipo string.
+$exite = true; //Se declara una variable doolean.
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre;echo "<br>";
+echo "variable doolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "mundo";
+echo $cadena1."     ".$cadena2;
+
+?>
