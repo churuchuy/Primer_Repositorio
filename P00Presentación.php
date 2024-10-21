36819dd06a8227f55c80934b9e275b6947832ea0
López Rojas Jesus Emiliano <jesus.lopez.rojas@cbtis89.edu.mx>
2024-10-21 09:31:48 -0600

    Add files via upload

---
+++ P00Presentación.php
@@ -0,0 +1,12 @@
+
+<?php
+/*CBTIS 89
+Programa que contiene mis datos personales
+Lopez Rojas Jesus Emiliano
+3ºA programacion Matutino
+*/
+echo "Mi nombre es: López Rojas Jesus Emiliano";
+echo "<br>","Estudio en el CBTIS 89 ";
+echo "<br>","en 3er semestre en la carrera de Programación A turno Matutino";
+echo "<br>","vivo en la ciudad de Durango,Dgo.";
+?>
